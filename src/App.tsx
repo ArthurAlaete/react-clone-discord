e6abb2c454621d6d9c1663b11d8d4f49922f8cab
ArthurAlaete <arthuralaetelopes@gmail.com>
2021-04-07 18:57:26 -0300

    Configuracoes e GlobalStyle

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <h1>Clone Discord</h1>
+
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
